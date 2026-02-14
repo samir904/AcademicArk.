0eb9f7f74bef1aab61428625580def1451beda4d
samir904 <sumansamir3@gmail.com>
2026-02-14 16:00:32 +0530

    fixed import issue

--- ROUTES/admin.paywall.routes.js
+++ ROUTES/admin.paywall.routes.js
@@ -6,9 +6,8 @@ import {
   getTopConvertingNotes,
   getMostPaywalledNotes
 } from "../CONTROLLERS/admin.paywall.controller.js";
-
+import {isLoggedIn,authorizedRoles}  from '../MIDDLEWARES/auth.middleware.js'
 const router = express.Router();
-import { authorizedRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
 
 // 🔐 Only ADMIN
 router.use(isLoggedIn, authorizedRoles("ADMIN"));
